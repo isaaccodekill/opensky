94d64083a85d0b9c8d110632d7caa6d45041c020
isaaccodekill <isaacbello3200@gmail.com>
2019-11-07 05:41:09 +1345

    Added the utilites folder

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,8 +8,8 @@ import Result from '../../Components/UI/Result/Result'
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem'
 import Select from '@material-ui/core/Select'
+import cities from '../../Utils/cities'
 
-// import Loader from ''
 
 
 const Home = () => {
@@ -49,78 +49,6 @@ const Home = () => {
 	}
 
 
-	const cities = [
-		{
-			id: 1,
-			name: "Hartsfield-Jackson Altlanta international airport",
-			Location: "Atlanta, Georgia",
-			image: "https://images.unsplash.com/photo-1504983376254-592a0ea9b0cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=891&q=80",
-			code: "KATL"
-		},
-		{
-			id: 2,
-			name: "O'Hare international	airport",
-			Location: "chicago, illinois",
-			image: "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=813&q=80",
-			code: "KORD"
-		},
-		{
-			id: 3,
-			name: "Los Angeles international airport",
-			Location: "California",
-			image: "https://images.unsplash.com/photo-1512746133319-6559ec0c1f39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=786&q=80",
-			code: "KLAX"
-		},
-		{
-			id: 4,
-			name: "Dallas/Fort Worth international airport",
-			Location: "Dallas, Texas",
-			image: "https://images.unsplash.com/photo-1542482780641-7f549ee20bc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
-			code: "KDFW"
-		},
-		{
-			id: 5,
-			name: "John F Kennedy international	airport",
-			Location: "New York",
-			image: "https://images.unsplash.com/photo-1532960401447-7dd05bef20b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=379&q=80",
-			code: "KJFK"
-		},
-		{
-			id: 6,
-			name: "Denver international	airport",
-			Location: "Denver, colorado",
-			image: "https://images.unsplash.com/photo-1507070989769-45210775a437?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=605&q=80",
-			code: "KDEN"
-		},
-		{
-			id: 7,
-			name: "San Francisco international airport",
-			Location: "California",
-			image: "https://images.unsplash.com/photo-1461696114087-397271a7aedc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-			code: "KSFO"
-		},
-		{
-			id: 8,
-			name: "McCarran international airport	",
-			Location: "Las Vegas, Nevada",
-			image: "https://images.unsplash.com/photo-1516975698824-571e2c952dbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-			code: "KLAS"
-		},
-		{
-			id: 9,
-			name: "Pheonix Sky Harbour international airport",
-			Location: "Phoenix Arizona",
-			image: "https://images.unsplash.com/photo-1437153225860-b850e2a4d0fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-			code: "KPHX"
-		},
-		{
-			id: 10,
-			name: "Goerge Bush intercontinental airport",
-			Location: "Houston, Texas",
-			image: "https://images.unsplash.com/photo-1470082719408-b2843ab5c9ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=891&q=80",
-			code: "KIAH"
-		},
-	]
 
 	const getFlightdetails = () => {
 		console.log(CURRENT_TIME_IN_SECS)
@@ -239,7 +167,7 @@ const Home = () => {
 									<MenuItem value="Days">Days</MenuItem>
 								</Select>
 								</div>
-								
+
 								<Button disabled={btnDisabled} onClick={() => {
 									getFlightdetails()
 								}} variant="contained" color="primary" size="medium" className={styles.button}>
